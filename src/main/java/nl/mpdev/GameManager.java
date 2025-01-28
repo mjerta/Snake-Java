35cab6f70a33f34dbc6008ad791adda41378e8d4
mjerta <maarten.postma1000@gmail.com>
2025-01-28 23:30:55 +0100

    Complete contructor with the the inititation of of the frame

--- src/main/java/nl/mpdev/GameManager.java
+++ src/main/java/nl/mpdev/GameManager.java
@@ -12,14 +12,27 @@ public class GameManager extends JFrame {
   private Timer timer;
 
   private GameManager() throws HeadlessException {
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(1000, 1000);
+    this.setTitle("Snake (Java)");
+    this.setLayout(new BorderLayout()); // Testing with layout
+
+    JPanel panel1 = new JPanel();
+    panel1.setBackground(Color.RED);
+
+    JPanel panel2 = new JPanel();
+    panel2.setBackground(Color.GREEN);
+
+    this.add(panel1, BorderLayout.NORTH);
+    this.add(panel2, BorderLayout.CENTER);
+
   }
 
-
   public static GameManager getInstance() {
-    if(INSTANCE == null) {
+    if (INSTANCE == null) {
       INSTANCE = new GameManager();
     }
-    return  INSTANCE;
+    return INSTANCE;
   }
 
 }
