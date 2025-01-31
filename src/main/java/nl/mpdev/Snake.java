3dc79f000cba1ddba45a4a14487bba748c0c7573
mjerta <maarten.postma1000@gmail.com>
2025-01-31 21:47:55 +0100

    Created the field and contructor

--- src/main/java/nl/mpdev/Snake.java
+++ src/main/java/nl/mpdev/Snake.java
@@ -1,4 +1,13 @@
 package nl.mpdev;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Snake {
+
+  List<Segment> body;
+
+  public Snake() {
+    this.body = new LinkedList<>();
+  }
 }
