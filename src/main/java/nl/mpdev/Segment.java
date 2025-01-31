045d94cd209fac5af59b5e4f9e88ae94266dfd23
mjerta <maarten.postma1000@gmail.com>
2025-01-31 22:00:04 +0100

    Add encapsulation and getters for Segment

--- src/main/java/nl/mpdev/Segment.java
+++ src/main/java/nl/mpdev/Segment.java
@@ -1,4 +1,20 @@
 package nl.mpdev;
 
 public class Segment {
+
+  private final int x;
+  private final int y;
+
+  public Segment(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
 }
