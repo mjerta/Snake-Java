c9e8dba9e35f0dac7ac7a8ae58a1b1ba537fb7b7
mjerta <maarten.postma1000@gmail.com>
2025-02-02 10:56:36 +0100

    Add encapsulation and methods necessary for the Apple class

--- src/main/java/nl/mpdev/Apple.java
+++ src/main/java/nl/mpdev/Apple.java
@@ -1,4 +1,21 @@
 package nl.mpdev;
 
+import java.awt.*;
+
 public class Apple {
+
+  private int x;
+  private int y;
+
+  public Apple(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public void respawn(){}
+
+  public void draw(Graphics g, double cellsize) {
+    g.setColor(Color.RED);
+    g.fillOval(x, y, (int) cellsize, (int) cellsize);
+  }
 }
