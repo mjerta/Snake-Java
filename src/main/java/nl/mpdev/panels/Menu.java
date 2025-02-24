91b556133725e6d5c09a5b76a4d84412890e3284
Maarten PostmaH <info@mpdev.nl>
2025-02-24 17:11:21 +0100

    Added some fields into the menu panel

--- src/main/java/nl/mpdev/panels/Menu.java
+++ src/main/java/nl/mpdev/panels/Menu.java
@@ -1,6 +1,20 @@
 package nl.mpdev.panels;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Menu extends JPanel {
+  private JPanel inputPanel;
+  private JTextField nameField;
+  private JButton submitButton;
+
+  public Menu() {
+    this.inputPanel = new JPanel();
+    this.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+    this.nameField = new JTextField(13);
+    this.submitButton = new JButton("Submit");
+  }
+
 }
