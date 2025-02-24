0a2d5fee45189331f1f61a186fc72f1b26f9651b
Maarten PostmaH <info@mpdev.nl>
2025-02-24 22:13:37 +0100

    Created a method for formatting the 2 inputs of the menu class

--- src/main/java/nl/mpdev/panels/Menu.java
+++ src/main/java/nl/mpdev/panels/Menu.java
@@ -1,7 +1,14 @@
 package nl.mpdev.panels;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Insets;
+import java.awt.TextField;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -12,9 +19,23 @@ public class Menu extends JPanel {
 
   public Menu() {
     this.inputPanel = new JPanel();
+    this.inputPanel.setBackground(Color.RED);
     this.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
     this.nameField = new JTextField(13);
     this.submitButton = new JButton("Submit");
+    formatInputs(nameField, new Insets(10, 20, 10, 20));
+    formatInputs(submitButton, new Insets(20, 20, 10, 20);
+  }
+
+  private <T extends JComponent> void formatInputs(T component, Insets margins) {
+    component.setFont(new Font("Arial", Font.PLAIN, 30));
+    if(component instanceof JButton) {
+      ((JButton) component).setMargin(margins);
+    } else if (component instanceof JTextField) {
+      ((JTextField) component).setMargin(margins);
+    }
+    nameField.setMaximumSize(nameField.getPreferredSize());
+    nameField.setAlignmentX(Component.CENTER_ALIGNMENT);
   }
 
 }
