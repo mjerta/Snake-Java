a6d7396a79f6f4f94e86cd068b730d7ec857959c
Maarten PostmaH <info@mpdev.nl>
2025-03-04 11:55:22 +0100

    Format the input some more

--- src/main/java/nl/mpdev/panels/Menu.java
+++ src/main/java/nl/mpdev/panels/Menu.java
@@ -35,7 +35,7 @@ public class Menu extends JPanel implements ActionListener, KeyListener {
     this.nameField = new JTextField(13);
     this.submitButton = new JButton("Submit");
     formatInputs(nameLabel,1,Color.WHITE, 20, 20, 10, 20);
-    formatInputs(nameField,1, Color.WHITE, 20, 20, 10, 20);
+    formatInputs(nameField,0, Color.BLACK, 20, 20, 10, 20);
     formatInputs(submitButton,1, Color.BLACK, 20, 20, 10, 20);
     formatPanel();
     submitButton.addActionListener(this);
@@ -46,18 +46,21 @@ public class Menu extends JPanel implements ActionListener, KeyListener {
   private <T extends JComponent> void formatInputs(T component, int fontStyle, Color color, int... margins) {
     component.setFont(new Font("Arial", fontStyle, 30));
     component.setForeground(color);
-    component.setMaximumSize(component.getPreferredSize());
     component.setAlignmentX(Component.CENTER_ALIGNMENT);
     if (margins.length == 4) {
       Insets insets = new Insets(margins[0], margins[1], margins[2], margins[3]);
       switch (component) {
         case JButton jButton -> jButton.setMargin(insets);
-        case JTextField jTextField -> jTextField.setMargin(insets);
+        case JTextField jTextField -> {
+          jTextField.setMargin(insets);
+          jTextField.setHorizontalAlignment(JTextField.CENTER);
+        }
         case JLabel jLabel -> jLabel.setBorder(new EmptyBorder(margins[0], margins[1], margins[2], margins[3]));
         default -> {
         }
       }
     }
+    component.setMaximumSize(component.getPreferredSize());
   }
 
   private void formatPanel() {
