6dd37cc0d33de582bebb1fcb715e961bf77018c8
Maarten PostmaH <info@mpdev.nl>
2025-03-07 16:25:54 +0100

    Add field and constructor for instructions

--- src/main/java/nl/mpdev/panels/Instructions.java
+++ src/main/java/nl/mpdev/panels/Instructions.java
@@ -1,8 +1,16 @@
 package nl.mpdev.panels;
 
+import java.awt.BorderLayout;
+import java.util.List;
+import java.util.Map;
+
 import javax.swing.JPanel;
 
 public class Instructions extends JPanel {
+  private Map<List, List> instructionLines;
 
-   
+  public Instructions() {
+    this.setLayout(new BorderLayout());
+    
+  }
 }
