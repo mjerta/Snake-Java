5b7bad72dc14bc55c75fb2bdef5cf4e70f5f573d
Maarten Postma <info@mpdev.nl>
2025-03-09 23:00:54 +0100

    In the progress of using keylistener inside InstructionsPanel

--- src/main/java/nl/mpdev/panels/InstructionPanel.java
+++ src/main/java/nl/mpdev/panels/InstructionPanel.java
@@ -1,23 +1,51 @@
 package nl.mpdev.panels;
 
 import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.swing.JPanel;
 
-public class InstructionPanel extends JPanel implements OverlayHandler {
+import nl.mpdev.GameManager;
+
+public class InstructionPanel extends JPanel implements OverlayHandler, KeyListener {
   private Map<List, List> instructionLines;
 
   public InstructionPanel() {
     this.setLayout(new BorderLayout());
     this.setVisible(false);
+    this.addKeyListener(this);
     instructionLines = new HashMap<>();
   }
 
   @Override
   public void activatePanel() {
     this.setVisible(true);
+    this.requestFocusInWindow();
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+    System.out.println("test");
+    System.out.println(e.getKeyCode());
+    if (e.getKeyCode() == 'i') {
+      Grid grid = GameManager.getInstance().getGrid();
+      grid.setKeysEnabled(true);
+      this.setVisible(false);
+      grid.setRunning(true);
+      grid.getTimer().start();
+    }
+
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
   }
 }
