ee6c3ea4216cb3ead2c68d97ce50ab321b0ba9fb
Maarten PostmaH <info@mpdev.nl>
2025-03-09 21:48:40 +0100

    Add interface and its method to instructionPanel

--- src/main/java/nl/mpdev/panels/InstructionPanel.java
+++ src/main/java/nl/mpdev/panels/InstructionPanel.java
@@ -7,12 +7,17 @@ import java.util.Map;
 
 import javax.swing.JPanel;
 
-public class InstructionPanel extends JPanel {
+public class InstructionPanel extends JPanel implements OverlayHandler {
   private Map<List, List> instructionLines;
 
   public InstructionPanel() {
     this.setLayout(new BorderLayout());
-    this.setVisible(false);    
+    this.setVisible(false);
     instructionLines = new HashMap<>();
+  }
+
+  @Override
+  public void activatePanel() {
+    this.setVisible(true);
   }
 }
